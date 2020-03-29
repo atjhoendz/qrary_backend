23754cfedb61d1ea49b9781b32b24d5458f3d266
atjhoendz <achunarmando@gmail.com>
2020-03-29 21:02:05 +0700

    update error code and mesage response

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -17,7 +17,7 @@ module.exports = {
             email: req.body.email,
             npm: req.body.npm
         }).then(hasil => {
-            if(hasil == null){
+            if (hasil == null) {
                 bcrypt.hash(req.body.pwd, round).then(hashed => {
                     User.create({
                         nama: req.body.nama,
@@ -28,11 +28,11 @@ module.exports = {
                         urlFoto: setUrlFoto(req.body.npm)
                     }).then(user => {
                         res.status(200).json(
-                            FormatResponse(true, 200, user, 'Registrasi berhasil', true)
+                            FormatResponse(true, 200, user, 'Pendaftaran berhasil', true)
                         );
                     }).catch(err => {
-                        res.status(500).json(
-                            FormatResponse(false, 500, "", `Registrasi gagal, ${err.message}`, true)
+                        res.status(200).json(
+                            FormatResponse(false, 200, "", `Pendaftaran tidak berhasil, ${err.message}`, true)
                         );
                     });
                 }).catch(err => {
@@ -40,9 +40,9 @@ module.exports = {
                         FormatResponse(false, 500, "", `Hashing pwd gagal, ${err.message}`, true)
                     );
                 });
-            }else{
-                res.status(500).json(
-                    FormatResponse(false, 500, "", 'Registrasi gagal, data sudah tersedia', true)
+            } else {
+                res.status(200).json(
+                    FormatResponse(true, 200, "", 'Pendaftaran tidak berhasil, data sudah tersedia', true)
                 );
             }
         }).catch(err => {
@@ -56,7 +56,7 @@ module.exports = {
             npm: req.body.npm
         }).orFail().then(user => {
             const isValid = bcrypt.compareSync(req.body.pwd, user.pwd);
-            if(isValid){
+            if (isValid) {
                 data = {
                     nama: user.nama,
                     npm: user.npm,
@@ -66,19 +66,19 @@ module.exports = {
                     loggedin: true
                 };
                 const token = jwt.sign(data, process.env.SECRET_KEY);
-                if(token){
+                if (token) {
                     res.status(200).json(
-                        FormatResponse(true, 200, token, 'Login berhasil', true)
+                        FormatResponse(true, 200, token, 'Anda berhasil masuk', true)
                     );
                 }
-            }else {
-                res.status(401).json(
-                    FormatResponse(false, 401, "", 'Login tidak berhasil, npm atau password salah', true)
+            } else {
+                res.status(200).json(
+                    FormatResponse(true, 200, "", 'Anda tidak berhasil masuk, kata sandi salah', true)
                 );
             }
         }).catch(err => {
-            res.status(401).json(
-                FormatResponse(false, 401, "", 'Login tidak berhasil, NPM belum terdaftar', true)
+            res.status(200).json(
+                FormatResponse(true, 200, "", 'Anda tidak berhasil masuk, NPM belum terdaftar', true)
             );
         });
     }
