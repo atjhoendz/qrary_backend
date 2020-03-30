56446a9b0805aee38783a171d649313b822f0213
atjhoendz <achunarmando@gmail.com>
2020-03-30 23:00:40 +0700

    add route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { getAll, find, deleteUser, update, updatePassword, updateRole } = require('../controllers/UserController');
-const { isContainReqData } = require('../middlewares/auth');
+const { getAll, getPaginate, find, deleteUser, update, updatePassword, updateRole } = require('../controllers/UserController');
+const { isContainReqData, isPageaNumber } = require('../middlewares/auth');
 
 router.get('/getAll', getAll);
+router.get('/page/:page/limit/:limit', isPageaNumber, getPaginate);
 router.get('/find/:key/:value', find);
 router.delete('/delete/:id', deleteUser);
 router.put('/update/:id', isContainReqData, update);
