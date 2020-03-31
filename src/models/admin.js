e6938748e0da8ff87d567593edae127af236d025
atjhoendz <achunarmando@gmail.com>
2020-03-31 22:13:33 +0700

    add admin model

---
+++ src/models/admin.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AdminSchema = new Schema({
+    nama: {
+        type: String,
+        required: true
+    },
+    nip: {
+        type: String,
+        required: true
+    },
+    pwd: {
+        type: String,
+        required: true
+    },
+    jabatan: {
+        type: String
+    },
+    jk: {
+        type: String,
+        required: true
+    },
+    tanggalLahir: {
+        type: Date
+    },
+    alamat: {
+        type: String
+    },
+    role: {
+        type: String,
+        required: true
+    },
+    urlFoto: {
+        type: String
+    }
+});
+
+const Admin = mongoose.model("Admin", AdminSchema, "Admin");
+module.exports = Admin;
