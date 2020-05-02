3eb8c42e1d8e7062a4d655aed7b0966fb99e81ea
atjhoendz <achunarmando@gmail.com>
2020-05-02 22:45:39 +0700

    add some option

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = () => {
-    return mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/qrary', { useUnifiedTopology: true, useCreateIndex: true, useNewUrlParser: true});
+    return mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/qrary', { useUnifiedTopology: true, useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false });
 };
 
 module.exports = connectDB;
