cf15980f34ec0016294ca693f893582360e58470
atjhoendz <achunarmando@gmail.com>
2020-12-14 22:42:29 +0700

    refactor connection to auth

--- src/utils/connection.js
+++ src/utils/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = () => {
-    return mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/qrary', { useUnifiedTopology: true, useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false });
+    return mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/qrary', { useUnifiedTopology: true, useCreateIndex: true, useNewUrlParser: true, useFindAndModify: false , user: process.env.MONGODB_USERNAME, pass: process.env.MONGODB_PASSWORD});
 };
 
 module.exports = connectDB;
