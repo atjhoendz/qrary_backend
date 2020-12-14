d3074dddcbbd61c24055ea0fb399d18357227fb9
atjhoendz <achunarmando@gmail.com>
2020-12-14 15:13:50 +0700

    refactor method to PUT

--- src/routes/peminjaman.js
+++ src/routes/peminjaman.js
@@ -7,6 +7,6 @@ router.post('/', isContainReqData, saveFromTemp); // /peminjaman/
 router.get('/', getAll); // /peminjaman/
 router.get('/paginate', getPaginate); // /peminjaman/paginate?page=1&limit=6
 router.get('/find', isQueryValid, find); // /peminjaman/find?key=_id&value=iniidpeminjamannya
-router.delete('/', isContainReqData, pengembalian); // /peminjaman/
+router.put('/', isContainReqData, pengembalian); // /peminjaman/
 
 module.exports = router;
